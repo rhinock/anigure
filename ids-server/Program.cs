3f258f887f3dbe4a9c4ab2cccf12774609dd4029
rhinock <rhinock@yandex.ru>
2023-02-12 13:46:52 +0400

    + Step 4: Add Quick start UI from Duende Quick start UI

--- ids-server/Program.cs
+++ ids-server/Program.cs
@@ -26,12 +26,28 @@ builder.Services.AddIdentityServer()
         }
     })
     .AddInMemoryIdentityResources(new List<IdentityResource>())
-    .AddTestUsers(new List<TestUser>());
+    .AddTestUsers(new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "Alice",
+            Username = "alice",
+            Password = "alice"
+        }
+    });
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.UseIdentityServer();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
 app.Run();
