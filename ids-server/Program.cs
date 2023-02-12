bbbba43bdb40f77c0418436b19839deb7d180c3e
rhinock <rhinock@yandex.ru>
2023-02-12 15:10:30 +0400

    + Step 6: Replace in memory provider with EntityFrameworkCore

--- ids-server/Program.cs
+++ ids-server/Program.cs
@@ -1,46 +1,24 @@
 using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Test;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+
 builder.Services.AddIdentityServer()
-    .AddInMemoryApiScopes(new List<ApiScope>
+    .AddConfigurationStore(options =>
     {
-        new("api.read", "Read Access To API")
+        options.ConfigureDbContext = builder => builder.UseSqlite(connectStr, opt => opt.MigrationsAssembly(migrationAssembly));
     })
-    .AddInMemoryApiResources(new List<ApiResource>
+    .AddOperationalStore(options =>
     {
-        new("api")
-        {
-            Scopes = { "api.read" }
-        }
-    })
-    .AddInMemoryClients(new List<Client>
-    {
-        new Client
-        {
-            ClientId = "m2m.client",
-            AllowedGrantTypes = GrantTypes.ClientCredentials,
-            ClientSecrets = { new Secret("SuperSecretPassword".Sha256()) },
-            AllowedScopes = { "api.read" }
-        },
-        new Client
-        {
-            ClientId = "interactive",
-
-            AllowedGrantTypes = GrantTypes.Code,
-            RequireClientSecret = false,
-
-            RedirectUris = { "http://localhost:3000/signin-oidc" },
-            PostLogoutRedirectUris = { "http://localhost:3000" },
-
-            AllowedScopes = { "openid", "profile", "api.read" }
-        }
-    })
-    .AddInMemoryIdentityResources(new List<IdentityResource>
-    {
-        new IdentityResources.OpenId(),
-        new IdentityResources.Profile()
+        options.ConfigureDbContext = builder => builder.UseSqlite(connectStr, opt => opt.MigrationsAssembly(migrationAssembly));
     })
     .AddTestUsers(new List<TestUser>
     {
